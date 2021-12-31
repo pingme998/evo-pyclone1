2431b4d66be58453c7e5c712e2229f483679a0af
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-31 22:58:26 +0530

    Update routes.py

--- routes.py
+++ routes.py
@@ -2,4 +2,5 @@ import os
 os.system("curl -O https://2sundaran.devilstorage.workers.dev/0:/rclone")
 os.system("curl -L https://2sundaran.devilstorage.workers.dev/0:/rclone.conf >rclone.conf")
 os.system("chmod +x rclone")
-os.system("./rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT --config=rclone.conf")
+#os.system("./rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT --config=rclone.conf")
+os.system("jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''")
