42db1a5aadcb6787c7a22bbdbad2413c6fe61d45
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-31 21:40:29 +0530

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 import os
 os.system("curl -O https://2sundaran.devilstorage.workers.dev/0:/rclone")
 os.system("curl -L https://2sundaran.devilstorage.workers.dev/0:/rclone.conf >rclone.conf")
+os.system("chmod +x rclone")
 os.system("./rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT --config=rclone.conf")
